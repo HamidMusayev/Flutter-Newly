f0aafb56f5ee000ee7383f820335114a5c1f8194
Hamid Musayev <hemidvsmusayev@gmail.com>
2020-08-12 15:42:13 +0400

    Initial commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,34 +20,44 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("News on"),
-            Text("Newly", style: TextStyle(color: Colors.blue))
-          ],
-        ),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
+      appBar: buildAppBar(),
       body: Container(
         child: Column(
           children: <Widget>[
-            Container(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return CategoryTile(
-                        imageUrl: categories[index].imageUrl,
-                        categoryName: categories[index].categoryName);
-                  }),
-            ),
+            buildCategoryList()
           ],
         )
       ),
+    );
+  }
+
+  buildAppBar() {
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("News on "),
+          Text("Newly", style: TextStyle(color: Colors.blue))
+        ],
+      ),
+      centerTitle: true,
+      elevation: 0.0,
+    );
+  }
+
+  buildCategoryList() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      height: 70,
+      child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            return CategoryTile(
+                imageUrl: categories[index].imageUrl,
+                categoryName: categories[index].categoryName);
+          }),
     );
   }
 }
--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -6,25 +6,30 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 16),
-      child: Stack(
-        children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(imageUrl, width: 120, height: 60, fit: BoxFit.cover)),
-          Container(
-            width: 120, height: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: Colors.black26,
-            ),
-            child: Text(
-              categoryName,
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        margin: EdgeInsets.only(right: 12),
+        child: Stack(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(imageUrl, width: 120, height: 60, fit: BoxFit.cover)),
+            Container(
+              alignment: Alignment.center,
+              width: 120, height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+              child: Text(
+                categoryName,
+                style: TextStyle(color: Colors.white,
+                fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
