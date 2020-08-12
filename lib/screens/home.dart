395070e6ed829813760d79f6fa3205b9516e5802
Hamid Musayev <hemidvsmusayev@gmail.com>
2020-08-12 15:24:45 +0400

    Initial commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/helper/data.dart';
+import 'package:news_app/models/category.dart';
+import 'package:news_app/widgets/category_tile.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,19 +9,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = List<CategoryModel>();
+
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("News on"), Text("Newly", style: TextStyle(color: Colors.blue),)],
+          children: [
+            Text("News on"),
+            Text("Newly", style: TextStyle(color: Colors.blue))
+          ],
         ),
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                        imageUrl: categories[index].imageUrl,
+                        categoryName: categories[index].categoryName);
+                  }),
+            ),
+          ],
+        )
+      ),
     );
   }
 }
-
--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -7,10 +7,23 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(right: 16),
       child: Stack(
         children: [
-          Image.network(imageUrl, width: 120, height: 60),
-
+          ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(imageUrl, width: 120, height: 60, fit: BoxFit.cover)),
+          Container(
+            width: 120, height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
+            child: Text(
+              categoryName,
+              style: TextStyle(color: Colors.white),
+            ),
+          )
         ],
       ),
     );
